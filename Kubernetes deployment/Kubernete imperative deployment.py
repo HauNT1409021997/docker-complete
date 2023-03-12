41561aeadd3434dbf8b848cd419f09e05d370fd8
MyPC <akitosama12546@gmail.com>
2023-03-11 23:19:04 -0800

    kubernetes deployment

---
+++ Kubernetes deployment/Kubernete imperative deployment.py
@@ -0,0 +1,3 @@
+#to create kubernetes deloyment
+#use command kubectl create deployment (object_name) test --image=(image_name) kubectdelpoyv1
+#to delete the deployment use kubectl delete (deployment_name) test
